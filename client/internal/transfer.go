1dd55d92b42d22d398b41523b6c96111209f86dd
Abhinav Sharma <abhinavs1920bpl@gmail.com>
2025-05-29 15:44:16 +0530

    refactor: add Transfer methods for pause/resume functionality
    Signed-off-by: Abhinav Sharma <abhinavs1920bpl@gmail.com>

--- client/internal/transfer.go
+++ client/internal/transfer.go
@@ -1,7 +1,12 @@
 package connection
 
 import (
-
+	"drizlink/utils"
+	"net"
+	"os"
+	"strconv"
+	"sync"
+	"time"
 )
 
 type TransferType int
@@ -46,3 +51,90 @@ func (s TransferStatus) String() string {
 	}
 }
 
+type TransferReader interface {
+	Read(p []byte) (n int, err error)
+	GetBytesProcessed() int64
+}
+
+type TransferWriter interface {
+	Write(p []byte) (n int, err error)
+	GetBytesProcessed() int64
+}
+
+type Transfer struct {
+	ID            string
+	Type          TransferType
+	Name          string
+	Size          int64
+	BytesComplete int64
+	Status        TransferStatus
+	Direction     string 
+	Recipient     string
+	Path          string
+	Checksum      string
+	StartTime     time.Time
+	File          *os.File
+	Connection    net.Conn
+	ProgressBar   *utils.ProgressBar
+	pauseMutex    sync.Mutex
+	isPaused      bool
+}
+
+var (
+	ActiveTransfers   = make(map[string]*Transfer)
+	TransfersMutex    sync.RWMutex
+	transferIDCounter = 1
+	DefaultManager    *TransferManager
+)
+
+func init() {
+	DefaultManager = NewTransferManager()
+}
+
+type TransferManager struct {
+	transfers      map[string]*Transfer
+	mutex          sync.RWMutex
+	nextID         int
+}
+
+func NewTransferManager() *TransferManager {
+	return &TransferManager{
+		transfers: make(map[string]*Transfer),
+		nextID:    1,
+	}
+}
+
+func (tm *TransferManager) GenerateID() string {
+	tm.mutex.Lock()
+	defer tm.mutex.Unlock()
+	id := strconv.Itoa(tm.nextID)
+	tm.nextID++
+	return id
+}
+
+// GenerateTransferID creates a unique ID for a transfer (legacy function for compatibility)
+func GenerateTransferID() string {
+	return DefaultManager.GenerateID()
+}
+
+func (tm *TransferManager) Register(transfer *Transfer) {
+	tm.mutex.Lock()
+	defer tm.mutex.Unlock()
+	tm.transfers[transfer.ID] = transfer
+}
+
+// RegisterTransfer adds a new transfer to the tracking system (legacy function for compatibility)
+func RegisterTransfer(transfer *Transfer) {
+	DefaultManager.Register(transfer)
+	// Also maintain old behavior for backward compatibility
+	TransfersMutex.Lock()
+	defer TransfersMutex.Unlock()
+	ActiveTransfers[transfer.ID] = transfer
+}
+
+func (tm *TransferManager) Get(id string) (*Transfer, bool) {
+	tm.mutex.RLock()
+	defer tm.mutex.RUnlock()
+	transfer, exists := tm.transfers[id]
+	return transfer, exists
+}
